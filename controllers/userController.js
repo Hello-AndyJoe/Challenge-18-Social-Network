099723bf5dd7b7738a3549faf8c71754c6842e31
Hello-AndyJoe <andrew.joseph.roper@gmail.com>
2023-11-10 18:23:36 -0700

    Can add and read users and thoughts, not much else.

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,8 +3,8 @@ const { Thought, User } = require('../models');
 module.exports = {
   async seeAllUsers(req, res) {
     try {
-      const allUser = await User.find();
-      res.status(200).json(allUser);
+      const allUsers = await User.find();
+      res.status(200).json(allUsers);
     } catch (err) {
       res.status(500).json(err);
     }
